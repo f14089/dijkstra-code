45f39e3cb560ef1f4c3e2c0182420050b4b9938e
f14089 <f14089@gmail.com>
2015-08-10 12:29:19 +0800

    some test

--- main.c
+++ main.c
@@ -12,7 +12,8 @@ typedef struct Path_Node
 	int Node_value;
 	int adjacent_dist; /*It has many values probably.*/
 	int visited; /*This algorithm need to know whether the node is visted.*/
-	struct Path_Node * Nextnode;	
+	struct Path_Node * Nextnode;
+	struct Path_Node * next_vertex;	
 }Path_Node;
 int main()
 {
@@ -20,14 +21,18 @@ int main()
 	struct Path_Node *source = NULL; 
 	struct Path_Node *current = NULL;
 	struct Path_Node *previous = NULL;
-
+	FILE * fp;
+	fp = fopen("graph","r");
+	int count,temp;
 	int i=0;
-	while(i!=7)
+	while(fscanf(fp,"%d ",&temp)!=EOF)
 	{
 	current=(struct Path_Node*)malloc(sizeof(struct Path_Node));
 	current->seq = i;
-	current->Node_value = 0;
-	current->adjacent_dist=i;
+	if(source == NULL){
+	current->Node_value = 0;}
+	else {current->Node_value = 1000000;}
+	current->adjacent_dist=temp;
 	current->visited=0;
 	current->Nextnode = NULL;
 	if(source == NULL){source = current;}
@@ -36,7 +41,18 @@ int main()
 	i++;
 	previous=current;
 	}
-
+	current=source;
+	while(current!=NULL){
+	printf("%d ",current->Node_value);	
+	if(((current->seq+1) % 9 == 0) && (current->seq!=0)){printf("\n");}
+	current=current->Nextnode;
+	}
+	current=source;
+	while(current!=NULL){
+	printf("%d ",current->adjacent_dist);	
+	if(((current->seq+1) % 9 == 0) && (current->seq!=0)){printf("\n");}
+	current=current->Nextnode;
+	}
 
 
 }
